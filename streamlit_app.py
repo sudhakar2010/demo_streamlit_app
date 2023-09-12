3e6626c89439a4f586360aff1d1765d6787965dd
sudhakar2010 <sudhakar201094@gmail.com>
2023-09-12 21:55:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
 
+import streamlit
+
+streamlit.title("My parents New Healthy Diner")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
